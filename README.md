675cbba26f31d1fb9ce594b106837de8c6466791
Phil Wang <lucidrains@gmail.com>
2022-07-01 15:37:44 -0700

    first pass

--- README.md
+++ README.md
@@ -1,12 +1,43 @@
 <img src="./gss.png" width="400png"></img>
 
-## Gated State Spaces - Pytorch (wip)
+## Gated State Spaces - Pytorch
 
 Implementation of Gated State Spaces, from the paper <a href="https://arxiv.org/abs/2206.13947">Long Range Language Modeling via Gated State Spaces</a>, in Pytorch. In particular, it will contain the hybrid version containing local self attention with the long-range GSS.
 
 It will also contain a few more settings to compare state spaces to a sequence-wise GLU depthwise conv, and even simpler, a parameterized exponential moving average along the sequence dimension. So we get to the bottom of whether state spaces are worth it, or whether it is really all about the `O(L log(L))` FFT convolution trick. Results will be shared in the readme.
 
 I will also pit the GSS module against the <a href="https://github.com/lucidrains/panoptic-transformer">Path-X</a> challenge and see how well it does.
+
+## Install
+
+```bash
+$ pip install gated-state-spaces-pytorch
+```
+
+## Usage
+
+```python
+import torch
+from gated_state_spaces_pytorch import GSS
+
+gss = GSS(
+    dim = 512,                  # dimension
+    dim_expansion_factor = 4,   # hidden dimension (expansion factor x dim) = 2048
+    dss_kernel_N = 512,
+    dss_kernel_H = 256
+)
+
+x = torch.randn(1, 1024, 512)
+
+out = gss(x) # (1, 1024, 512)
+```
+
+## Todo
+
+- [ ] enwik8
+- [ ] gss lm class
+- [ ] gss + local attention lm class
+- [ ] add dsconv + learned ema
 
 ## Citations
 
--- gated_state_spaces_pytorch/gss.py
+++ gated_state_spaces_pytorch/gss.py
@@ -1,11 +1,106 @@
 import torch
 from torch import nn, einsum
+from torch.fft import rfft, irfft
 
-from einops import rearrange, repeat, reduce
+from einops import rearrange
 
-class GSS(nn.Module):
-    def __init__(self):
+# classes
+
+class DSS(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        kernel_N = 512
+    ):
         super().__init__()
+        self.norm = nn.LayerNorm(dim)
+
+        # Lambda
+
+        self.Lambda_real = nn.Parameter(torch.randn(kernel_N))
+        self.Lambda_imag = nn.Parameter(torch.randn(kernel_N))
+
+        # C
+
+        self.C_real = nn.Parameter(torch.randn(dim, kernel_N))
+        self.C_imag = nn.Parameter(torch.randn(dim, kernel_N))
+
+        # params D
+
+        self.param_D = nn.Parameter(torch.randn(dim))
 
     def forward(self, x):
-        return x
+        """
+        einstein notation:
+        b - batch
+        l - sequence length
+        d - dimension
+        """
+
+        device, seq_len = x.device, x.shape[1]
+        u = self.norm(x)
+
+        # learned weighted residual
+
+        residual = u * self.param_D
+
+        # derive simple dss kernel
+
+        Lambda = -self.Lambda_real.exp() + 1j * self.Lambda_imag.exp()
+        C = self.C_real + 1j * self.C_imag
+
+        arange = torch.arange(seq_len, device = device)
+
+        S = (rearrange(Lambda, 'n -> n 1') * rearrange(arange, 'l -> 1 l')).exp()
+        C = C * (Lambda.exp() - 1) / Lambda
+
+        K = einsum('h n, n l -> h l', C, S).real
+
+        # conv1d fft O(nlog(n))
+
+        u = rearrange(u, 'b l d -> b d l')
+
+        u_f = rfft(u, n = seq_len * 2, dim = -1)
+        K_f = rfft(K, n = seq_len * 2, dim = -1)
+
+        y = irfft(u_f * K_f, seq_len * 2, dim = -1)[..., :seq_len]
+        y = rearrange(y, 'b d l -> b l d')
+
+        return y + residual
+
+class GSS(nn.Module):
+    """ Pseudocode 3.2 """
+
+    def __init__(
+        self,
+        *,
+        dim,
+        dim_expansion_factor = 4,
+        dss_kernel_N = 512,
+        dss_kernel_H = 256
+    ):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+
+        dim_hidden = int(dim_expansion_factor * dim)
+        self.to_u = nn.Sequential(nn.Linear(dim, dim_hidden, bias = False), nn.GELU())
+        self.to_v = nn.Sequential(nn.Linear(dim, dss_kernel_H, bias = False), nn.GELU())
+
+        self.dss = DSS(dim = dss_kernel_H)
+
+        self.to_gate = nn.Linear(dss_kernel_H, dim_hidden, bias = False)
+        self.to_out = nn.Linear(dim_hidden, dim)
+
+    def forward(self, x):
+        residual, x = x.clone(), self.norm(x)
+
+        u = self.to_u(x)
+        v = self.to_v(x)
+
+        v = self.dss(v)
+
+        uc = self.to_gate(v)
+        out = self.to_out(uc * u)
+
+        return out + residual
